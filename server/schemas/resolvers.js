d084da3f82d7a2e606279cfdf9f261f8c036b2f2
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-02-11 16:44:15 -0500

    backend complete EXCEPT auth for some mutation routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,50 @@
-// const { Class } = require('../models');
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
-// const resolvers = {
-//   Query: {
-//     classes: async () => {
-//       return await Class.find({});
-//     }
-//   }
-// };
+const resolvers = {
+  Query: {
+    me: async (parent, { userId }) => {
+      return await User.findOne({ _id: userId});
+    },
+  },
+
+  Mutation: {
+    login: async (parent, {email,password}) => {
+      const user = await User.findOne({email:email});
+      if (!user) {
+        throw AuthenticationError
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw AuthenticationError
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+    addUser: async (parent, {username,email,password}) => {
+      const user = await User.create({username,email,password})
+      const token = signToken(user);
+
+      return {token,user}
+    },
+    // TODO: the functions below need a user identifier, in the original code it comes from a middleware when the token gets passed through the original API call
+    // do i just take the token as another param and then grab the user from that? can i just do that here?
+    saveBook: async (parent, body) => {
+      return await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: body } },
+        { new: true, runValidators: true }
+      );
+    },
+    removeBook: async (parent, {bookId}) => {
+      return await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+    },
+  }
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,35 @@
 const typeDefs = `
-  type Class {
+  type User {
     _id: ID
-    name: String
-    building: String
-    creditHours: Int
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    classes: [Class]
+    me(userId: ID!): [User]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String]!, description: String!, title: String!, bookId: ID!, image: String!, link: String!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
