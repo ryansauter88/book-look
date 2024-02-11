e76067218b32763bac27baf5e26b4b4496b06356
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-02-11 15:01:02 -0500

    initial commit, restructured server.js for graphql, added schemas folder

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+// Import the ApolloServer class
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+const { typeDefs, resolvers } = require('./schemas');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startApolloServer = async () => {
+  await server.start();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer();
